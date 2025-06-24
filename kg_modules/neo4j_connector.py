6d913b63fc9c3b92d8ca08c20ea1f4a54da61ac0
LeiYanDan <1274705490@qq.com>
2025-06-24 11:07:07 +0800

    查询 接口

--- kg_modules/neo4j_connector.py
+++ kg_modules/neo4j_connector.py
@@ -1,8 +1,14 @@
+# kg_modules/neo4j_connector.py
+
 from py2neo import Graph
 
 # Neo4j连接配置
 NEO4J_URL = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "testpassword"  # 和docker-compose.yml中设置的一致
+NEO4J_PASSWORD = "testpassword"
 
 graph = Graph(NEO4J_URL, auth=(NEO4J_USER, NEO4J_PASSWORD))
+
+def run_cypher(cypher_query, parameters=None):
+    result = graph.run(cypher_query, parameters or {})
+    return [dict(record) for record in result]
