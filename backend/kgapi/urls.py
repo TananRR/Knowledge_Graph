d772ac56bd512af6f67a6c228272903d86f2a4e2
LeiYanDan <1274705490@qq.com>
2025-06-24 16:20:49 +0800

    导出知识图谱

--- backend/kgapi/urls.py
+++ backend/kgapi/urls.py
@@ -1,7 +1,9 @@
 # kgapi/urls.py
 from django.urls import path
 from .views import search_entity  # 确保这是正确的导入
+from .export import export_knowledge_graph
 
 urlpatterns = [
     path('search/', search_entity),  # 确保这个路径存在
+    path('export/', export_knowledge_graph),
 ]
