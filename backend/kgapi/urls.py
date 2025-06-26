73d8375d744a25abd68de49c3458a74aeae89f71
LeiYanDan <1274705490@qq.com>
2025-06-26 14:40:01 +0800

    查询知识图谱

--- backend/kgapi/urls.py
+++ backend/kgapi/urls.py
@@ -4,6 +4,7 @@ from .views import search_entity  # 确保这是正确的导入
 from .export import export_knowledge_graph
 from .extract import extract_text_from_file
 from .delete import delete_all_graphs, delete_graph_by_id, delete_graphs_by_user
+from .query import get_graph_by_id, get_user_graph_ids, get_graphs_by_user, get_all_graphs
 
 urlpatterns = [
     path('search/', search_entity),  # 确保这个路径存在
@@ -12,4 +13,8 @@ urlpatterns = [
     path('delete/all/', delete_all_graphs),
     path('delete/graph/', delete_graph_by_id),
     path('delete/user/', delete_graphs_by_user),
+    path('query/graph/', get_graph_by_id),
+    path('query/user/graph_ids/', get_user_graph_ids, ),
+    path('query/user/graphs/', get_graphs_by_user),
+    path('query/all/graphs/', get_all_graphs),
 ]
